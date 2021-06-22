0e599078b737a56025ab2bead34c2c13d938ea17
viktorv9 <38245893+viktorv9@users.noreply.github.com>
2021-06-22 18:03:10 +0200

    small consistency fix
    rotation bug fix

--- src/logic/game/appletile.ts
+++ src/logic/game/appletile.ts
@@ -3,11 +3,14 @@ export class AppleTile {
 
     private x;
     private y;
-    private rotation = 0;
+    private rotation;
 
-    constructor(x : any, y : any) {
+    constructor(x : any, y : any, rotation = 0) {
         this.x = x;
         this.y = y;
+        this.rotation = rotation;
+
+        console.log(this.rotation)
     }
 
 }
